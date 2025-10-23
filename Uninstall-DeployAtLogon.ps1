75dcd4072f0248af8bdd7b67b5bb68e33f0f4368
j0eyv <41282854+j0eyv@users.noreply.github.com>
2025-10-23 20:09:37 +0200

    Add files via upload

---
+++ Uninstall-DeployAtLogon.ps1
@@ -0,0 +1,16 @@
+# MODIFYABLE PARAMETERS
+$taskName   = "Component_DeployAtLogon" # Define the name of the scheduled task
+
+# STATIC PARAMETERS
+$scriptDir  = "C:\ProgramData\CustomDeviceManagement\"
+
+# REMOVE SCHEDULED TASK
+if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false -ErrorAction SilentlyContinue
+}
+
+# REMOVE SCRIPT FILE
+$scriptFile = "$scriptDir$taskName.ps1"
+if (Test-Path $scriptFile) {
+    Remove-Item $scriptFile -ErrorAction SilentlyContinue
+}
